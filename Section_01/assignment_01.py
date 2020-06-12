dc9edfad635551efe26466d7c6a83c01f855428f
rohithm123 <51744983+rohithm123@users.noreply.github.com>
2020-06-12 10:34:38 +0530

    Update assignment_01.py

--- Section_01/assignment_01.py
+++ Section_01/assignment_01.py
@@ -1,10 +1,5 @@
 # Assignment 1:
-"""
-    We would like to get the remainder of 15 divided by 4.
-    The calculation below does not seem to give us this result.
-    How would you change the code to meet the requirement?
-
-"""
+print(15%4)
 
 
 
